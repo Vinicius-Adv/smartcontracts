f1cdbd1ba46df3cdefba0401fd5069f649edc8bf
Vinicius-Adv <84423516+Vinicius-Adv@users.noreply.github.com>
2021-07-06 03:19:17 -0300

    Create registroprocessos.sol

--- registroprocessos.sol
+++ registroprocessos.sol
@@ -6,9 +6,11 @@ pragma solidity 0.8.6;
 contract Registroprocessos {
 
     struct Processo {
-        string autor;
-        string reu;
+        string cliente;
+        string partecontraria;
+        string litisconsorcio;
         string causa;
+        string acordo;
         uint valordacausa;
         uint prazofinal;
     }
@@ -22,16 +24,21 @@ contract Registroprocessos {
     }
 
     function incluirProcesso(
-        string memory paramAutor,
-        string memory paramReu,
+        string memory paramCliente,
+        string memory paramPartecontraria,
+        string memory paramLitisconsorcio,
         string memory paramCausa,
+        string memory paramAcordo,
         uint paramValordacausa,
         uint paramPrazofinal
     ) external returns (bool) {
         require(msg.sender == owner, "So o dono pode fazer isso");
-        Processo memory novoProcesso = Processo(paramAutor, paramReu, paramCausa, paramValordacausa, paramPrazofinal);
+        Processo memory novoProcesso = Processo(paramCliente, paramPartecontraria, paramLitisconsorcio, paramCausa,
+        paramAcordo, paramValordacausa, paramPrazofinal);
         processos.push(novoProcesso);
         return true;
     }
+    
+}
     
 }
