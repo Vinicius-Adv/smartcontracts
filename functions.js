9edcde07fa7c01230eb5409190a19258e564bd54
Vinicius-Adv <84423516+Vinicius-Adv@users.noreply.github.com>
2021-07-06 03:20:13 -0300

    Create functions.js

--- functions.js
+++ functions.js
@@ -1,20 +1,24 @@
 async function getRegisterContractData() {
     let contractNumberField = document.getElementById("contractNumber");
     const userInput = contractNumberField.value * 1;
-    alert("valorInformadoPeloUsuario: " + userInput);
+    alert("Você busca pelo contrato " + userInput);
     try {
       const arrayProcessosData = await smartContract.processos(userInput);
       console.log(arrayProcessosData);
-      document.getElementById("autor").innerText = arrayProcessosData[0];
-      document.getElementById("reu").innerText = arrayProcessosData[1];
-      document.getElementById("causa").innerText = arrayProcessosData[2];
-      document.getElementById("valordacausa").innerText = arrayProcessosData[3];
-      document.getElementById("prazofinal").innerText = arrayProcessosData[4];
+      document.getElementById("cliente").innerText = arrayProcessosData[0];
+      document.getElementById("partecontraria").innerText = arrayProcessosData[1];
+      document.getElementById("litisconsorcio").innerText = arrayProcessosData[2];
+      document.getElementById("causa").innerText = arrayProcessosData[3];
+      document.getElementById("acordo").innerText = arrayProcessosData[4];
+      document.getElementById("valordacausa").innerText = arrayProcessosData[5];
+      document.getElementById("prazofinal").innerText = arrayProcessosData[6];
     } catch (err) {
       console.error(err);
-      document.getElementById("autor").innerText = "";
-      document.getElementById("reu").innerText = "";
+      document.getElementById("cliente").innerText = "";
+      document.getElementById("partecontraria").innerText = "";
+      document.getElementById("litisconsorcio").innerText = "";
       document.getElementById("causa").innerText = "";
+      document.getElementById("acordo").innerText = "";
       document.getElementById("valordacausa").innerText = "";
       document.getElementById("prazofinal").innerText = "";
       contractNumberField.value = 0;
@@ -29,6 +33,6 @@ async function getRegisterContractData() {
       document.getElementById("spanOwner").innerText = contractOwner;
     } catch (err) {
       console.error(err);
-      alert("Ué, cadê o Owner?");
+      alert("Ué, mas cadê o Owner?");
     }
   }
