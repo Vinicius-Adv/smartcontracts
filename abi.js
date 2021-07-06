d3cbe97e4750b98f5d5ae2a2af24ef3de10cefb0
Vinicius-Adv <84423516+Vinicius-Adv@users.noreply.github.com>
2021-07-06 03:21:34 -0300

    Create abi.js

--- abi.js
+++ abi.js
@@ -1,102 +1,123 @@
-const contractAddress = "0xC7bD2d9ef9A9b07DD29D498Ab1cBDf3EE93b1a62";
+const contractAddress = "0x346FE0E71AC1F85a0588642b05965A3d237B1425";
 var smartContract;
 var smartContractWithSigner;
 const ABI = 
 [
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "paramAutor",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "paramReu",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "paramCausa",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "paramValordacausa",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "paramPrazofinal",
-				"type": "uint256"
-			}
-		],
-		"name": "incluirProcesso",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"inputs": [],
-		"name": "owner",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "processos",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "autor",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "reu",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "causa",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "valordacausa",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "prazofinal",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	}
+
+		{
+			"inputs": [
+				{
+					"internalType": "string",
+					"name": "paramCliente",
+					"type": "string"
+				},
+				{
+					"internalType": "string",
+					"name": "paramPartecontraria",
+					"type": "string"
+				},
+				{
+					"internalType": "string",
+					"name": "paramLitisconsorcio",
+					"type": "string"
+				},
+				{
+					"internalType": "string",
+					"name": "paramCausa",
+					"type": "string"
+				},
+				{
+					"internalType": "string",
+					"name": "paramAcordo",
+					"type": "string"
+				},
+				{
+					"internalType": "uint256",
+					"name": "paramValordacausa",
+					"type": "uint256"
+				},
+				{
+					"internalType": "uint256",
+					"name": "paramPrazofinal",
+					"type": "uint256"
+				}
+			],
+			"name": "incluirProcesso",
+			"outputs": [
+				{
+					"internalType": "bool",
+					"name": "",
+					"type": "bool"
+				}
+			],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"stateMutability": "nonpayable",
+			"type": "constructor"
+		},
+		{
+			"inputs": [],
+			"name": "owner",
+			"outputs": [
+				{
+					"internalType": "address",
+					"name": "",
+					"type": "address"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "uint256",
+					"name": "",
+					"type": "uint256"
+				}
+			],
+			"name": "processos",
+			"outputs": [
+				{
+					"internalType": "string",
+					"name": "cliente",
+					"type": "string"
+				},
+				{
+					"internalType": "string",
+					"name": "partecontraria",
+					"type": "string"
+				},
+				{
+					"internalType": "string",
+					"name": "litisconsorcio",
+					"type": "string"
+				},
+				{
+					"internalType": "string",
+					"name": "causa",
+					"type": "string"
+				},
+				{
+					"internalType": "string",
+					"name": "acordo",
+					"type": "string"
+				},
+				{
+					"internalType": "uint256",
+					"name": "valordacausa",
+					"type": "uint256"
+				},
+				{
+					"internalType": "uint256",
+					"name": "prazofinal",
+					"type": "uint256"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		}
 ];
